a0f4ea0966d42ebe5b2677f90a347a47e868e4a4
Александр Геллерт <sever6131@ya.ru>
2020-02-14 17:00:10 +0600

    Добавьте файлы проекта.

---
+++ FileWorkApp/FileWork.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWorkApp
+{
+    
+    public class FileWork
+    {
+        private static string dirPath = AppDomain.CurrentDomain.BaseDirectory + @"Temp";
+        public static string Open()
+        {
+            OpenFileDialog openD = new OpenFileDialog
+            {
+                Title = "Выберите файл",
+                Multiselect = false,
+                InitialDirectory = dirPath,
+                Filter = "Textfile (*.txt) |*.txt;"
+            };
+
+            openD.ShowDialog();
+
+            return File.ReadAllText(openD.FileName);
+
+        }
+        public static void Save(string text)
+        {
+            SaveFileDialog saveD = new SaveFileDialog
+            {
+                Title = "Выберите папку для сохранения",
+                DefaultExt = "txt",
+                InitialDirectory = dirPath
+            };
+
+            saveD.ShowDialog();
+
+            File.AppendAllText(saveD.FileName, text, Encoding.Default);
+        }
+    }
+        
+}
---
+++ FileWorkApp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileWorkApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                textBox.Text = FileWork.Open();
+            }
+            catch (ArgumentException exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void SaveFile(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = textBox.Text;
+                FileWork.Save(text);
+            }
+            catch (ArgumentException exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
